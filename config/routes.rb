d4b50bfd79111d302ebd9ae29918929a53beffc6
cvangilder64 <cvangilder64@gmail.com>
2014-07-08 15:04:28 -0600

    sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-    
-    
-    resources :home
+
     root 'home#index'
+    resources :home
+    resources :signup
+
+
+
+
     
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
