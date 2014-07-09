00e6488fff23625b59bc6766a68926e13207a225
cvangilder64 <cvangilder64@gmail.com>
2014-07-09 15:19:17 -0600

    user new

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
 
+    validates :name, presence: true
+    validates :email, presence: true
+    has_secure_password
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140709204211) do
+ActiveRecord::Schema.define(version: 20140709211005) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140709204211) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password"
+    t.string   "password_digest"
   end
 
 end
