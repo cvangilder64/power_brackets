f144afd390f6cbe8ee244f6dbc83c925227b84ab
Connor Van Gilder <cvangilder64@gmail.com>
2014-07-09 19:56:19 -0600

    Users Table

---
+++ db/migrate/20140710015358_remove_columns_from_table.rb
@@ -0,0 +1,5 @@
+class RemoveColumnsFromTable < ActiveRecord::Migration
+  def change
+    remove_column :users, :password, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140709211005) do
+ActiveRecord::Schema.define(version: 20140710015358) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password"
     t.string   "password_digest"
   end
 
